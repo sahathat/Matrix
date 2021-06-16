43fdcfcf3358f335dc5dd6b6d9c8e02c525a92bb
sahathat <61311677+sahathat@users.noreply.github.com>
2021-06-16 12:59:55 +0700

    version add multiply and determinant

--- Matrix/src/matrix/Matrix.java
+++ Matrix/src/matrix/Matrix.java
@@ -1,68 +1,89 @@
-
 package matrix;
 
 public class Matrix {
 
-    public Matrix() {
+    private final int dimention;
+
+    public Matrix(int dimention) {
+        this.dimention = dimention;
     }
-    
-    public void printMatrix(int A[][]){
-        System.out.println("[ ");
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[i].length;j++){
+
+    public void printMatrix(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
                 System.out.print(A[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("]");
     }
-    
-    public void add(int A[][],int B[][]){
-        System.out.println("[ ");
-        int[][] Add = null;
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[i].length;j++){
-                System.out.print(A[i][j] + B[i][j] + " ");
+
+    public int[][] add(int[][] A, int[][] B) {
+        int[][] add = new int[dimention][dimention];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                add[i][j] = A[i][j] + B[i][j];
             }
-            System.out.println();
         }
-        System.out.println("]");
+        return add;
     }
-    
-    public void subAB(int A[][],int B[][]){
-        System.out.println("[ ");
-        int[][] Add = null;
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[i].length;j++){
-                System.out.print(A[i][j] - B[i][j] + " ");
+
+    public int[][] subAB(int[][] A, int[][] B) {
+        int[][] sub = new int[dimention][dimention];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                sub[i][j] = A[i][j] - B[i][j];
             }
-            System.out.println();
         }
-        System.out.println("]");
+        return sub;
     }
-    
-    public void subBA(int A[][],int B[][]){
-        System.out.println("[ ");
-        int[][] Add = null;
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[i].length;j++){
-                System.out.print(B[i][j] - A[i][j] + " ");
+
+    public int[][] transpose(int[][] A) {
+        int[][] transpose = new int[dimention][dimention];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                transpose[i][j] = A[j][i];
             }
-            System.out.println();
         }
-        System.out.println("]");
+        return transpose;
     }
-    
-    public void transpose(int A[][]){
-        System.out.println("[ ");
-        int[][] Transpose = null;
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[i].length;j++){
-                System.out.print(A[j][i] + " ");
+
+    public int[][] multiple(int[][] A, int[][] B) {
+        int[][] muit = new int[dimention][dimention];
+        for (int i = 0; i < dimention; i++) {
+            for (int j = 0; j < dimention; j++) {
+                for (int k = 0; k < dimention; k++) {
+                    muit[i][j] += A[i][k] * B[k][j];
+                }
             }
-            System.out.println();
         }
-        System.out.println("]");
+        return muit;
     }
-    
+
+    public int determinant(int[][] A) {
+        int result = 0;
+        if (A.length == 1) {
+            result = A[0][0];
+            return result;
+        }
+        if (A.length == 2) {
+            result = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+            return result;
+        }
+        for (int i = 0; i < A[0].length; i++) {
+            int temp[][] = new int[A.length - 1][A[0].length - 1];
+
+            for (int j = 1; j < A.length; j++) {
+                for (int k = 0; k < A[0].length; k++) {
+
+                    if (k < i) {
+                        temp[j - 1][k] = A[j][k];
+                    } else if (k > i) {
+                        temp[j - 1][k - 1] = A[j][k];
+                    }
+                }
+            }
+            result += A[0][i] * Math.pow(-1, (int) i) * determinant(temp);
+        }
+        return result;
+    }
 }
--- Matrix/src/matrix/MatrixTest.java
+++ Matrix/src/matrix/MatrixTest.java
@@ -6,20 +6,24 @@ public class MatrixTest {
     public static void main(String[] args) {
         int A[][] = {{1,2,3},{4,0,6},{7,8,9}};
         int B[][] = {{1,1,4},{0,2,3},{4,5,7}};
-        Matrix m=new Matrix();
+        Matrix m=new Matrix(3);
         System.out.println("\nMatrix A");
         m.printMatrix(A);
         System.out.println("\nMatrix B");
         m.printMatrix(B);
-        System.out.println("\nA transpose");
-        m.transpose(A);
+        System.out.println("\nA transpose ");
+        m.printMatrix(m.transpose(A));
+        
         System.out.println("\nB transpose");
-        m.transpose(B);
+        m.printMatrix(m.transpose(B));
+        
         System.out.println("\nA+B = ");
-        m.add(A, B);
+        m.printMatrix(m.add(A, B));
         System.out.println("\nA-B = ");
-        m.subAB(A, B);
-        System.out.println("\nB-A = ");
-        m.subBA(A, B);
+        m.printMatrix(m.subAB(A, B));
+        System.out.println("\nA*B = ");
+        m.printMatrix(m.multiple(A, B));
+        System.out.println("\ndet(A) = " + m.determinant(A));
+        System.out.println("\ndet(B) = " + m.determinant(B));
     }
 }
